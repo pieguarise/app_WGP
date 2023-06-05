adf8055d4f01b5dbe2ff874e78a4ca1dff147d3a
gloriacrestani <gloriacrestani@hotmail.it>
2023-06-05 10:59:54 +0200

    [wgp] first database

--- app_calorie/lib/database/database.dart
+++ app_calorie/lib/database/database.dart
@@ -8,7 +8,7 @@ import 'daos/couponsDao.dart';
 import 'entities/coupons.dart';
 
 //The generated code will be in database.g.dart
-part 'database.g.dart';
+//part 'database.g.dart';
 
 //Here we are saying that this is the first version of the Database and it has just 1 entity, i.e., Todo
 @Database(version: 1, entities: [Coupons])
